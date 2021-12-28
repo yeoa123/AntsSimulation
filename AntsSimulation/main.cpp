4805a4985176f265cfdee5fb6a376847a144c7da
Joel Naviliat <yeoa@gmx.de>
2021-12-28 17:31:16 +0100

    cleaned up the main.cpp and added comments

--- AntsSimulation/main.cpp
+++ AntsSimulation/main.cpp
@@ -10,6 +10,7 @@ bool state = false;
 
 int main()
 {
+    // SFML TEXT-DEBUG EXAMPLE
     /*
     sf::Text text;
     sf::Font font;
@@ -21,17 +22,15 @@ int main()
 
     sf::VertexArray dp = sf::VertexArray::VertexArray(sf::Points, 1);
     dp[0] = sf::Vertex::Vertex(sf::Vector2f(), sf::Color::Blue);
+    vec2f p = ants.debuggyPosition();
+    dp[0] = sf::Vertex::Vertex(sf::Vector2f(p.x, p.y), sf::Color::Red);
+
+    std::string s = std::to_string(p.x) + "  :  " + std::to_string(p.y);
+
+    text.setString(s);
+    text.setCharacterSize(20);
+    text.setFillColor(sf::Color::White);
     */
-    /*
-vec2f p = ants.debuggyPosition();
-dp[0] = sf::Vertex::Vertex(sf::Vector2f(p.x, p.y), sf::Color::Red);
-
-std::string s = std::to_string(p.x) + "  :  " + std::to_string(p.y);
-
-text.setString(s);
-text.setCharacterSize(20);
-text.setFillColor(sf::Color::White);
-*/
     
     
     // SFML setup stuff
@@ -50,11 +49,7 @@ text.setFillColor(sf::Color::White);
 
     // create and setup agents
     Agents ants1 = Agents(10000, 0x00FFFFFF);
-    Agents ants2 = Agents(10000, 0x00FFFFFF);
-    Agents ants3 = Agents(10000, 0x00FFFFFF);
     ants1.initialize("random");
-    ants2.initialize("random");
-    ants3.initialize("random");
 
     while (window.isOpen())
     {
@@ -70,16 +65,16 @@ text.setFillColor(sf::Color::White);
         float dt = clk.restart().sf::Time::asSeconds();
         
 
-        // change buffers and load vertex-array
+        // change buffers 
+        // update agents
+        // write onto new map
+        // fade map
+        // load map into vertex array
         if (state)
         {
             // B is current map, loading things into A
             ants1.update(dt, map_b);
-            ants2.update(dt, map_b);
-            ants3.update(dt, map_b);
             ants1.mapPositions(map_b);
-            ants2.mapPositions(map_b);
-            ants3.mapPositions(map_b);
             Animation::fadeMap1(map_a, map_b);
             Animation::updateVertexArray2(va, map_a);
             state = !state;
@@ -88,18 +83,14 @@ text.setFillColor(sf::Color::White);
         {
             // A is current map, loading things into B
             ants1.update(dt, map_a);
-            ants2.update(dt, map_a);
-            ants3.update(dt, map_a);
             ants1.mapPositions(map_a);
-            ants2.mapPositions(map_a);
-            ants3.mapPositions(map_a);
             Animation::fadeMap1(map_b, map_a);
             Animation::updateVertexArray2(va, map_b);
             state = !state;
         }
         
         
-
+        // do the window displaying stuff
         window.clear();
         window.draw(*va);
         window.display();
