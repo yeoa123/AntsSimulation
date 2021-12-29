2888cb9bac2b218cbc4b12a49a10c3b1ddd704ed
Joel Naviliat <yeoa@gmx.de>
2021-12-29 23:38:24 +0100

    cleaned up methods in Animation class

--- AntsSimulation/animation.cpp
+++ AntsSimulation/animation.cpp
@@ -17,57 +17,8 @@ void Animation::updateVertexArray(sf::VertexArray* va, datamap* map)
 	}
 };
 
-void Animation::fadeMap1(datamap *draw, datamap *constant)
-{
-	// this is simple gaussian 3x3 blur
-	// set draw-map to zero
-	draw->initData("zero");
-	// add (constant-map + offsets) * factor onto draw-map
-	for (int i = 0; i < WIDTH; i++)
-	{
-		for (int j = 0; j < HEIGHT; j++)
-		{
-			// setup some variables
-			unsigned int r = 0;
-			unsigned int g = 0;
-			unsigned int b = 0;
-
-			vec2i position = vec2i(i, j);
-			vec2i offset;
-
-			// get the values from constant with offset and factor
-			for (auto f = 0; f < filter_size; f++)
-			{
-				offset = position + filter[f];
-				if (datamap::inMap(offset))
-				{
-					unsigned int data = constant->getData(offset);
-					r += R(data);
-					g += G(data);
-					b += B(data);
-				}
-			}
-			// faktoring value with inverse kernel
-			r >>= 2;
-			g >>= 2;
-			b >>= 2;
-			// load value into draw
-			float dimm = 0.5f;
-			float foor = static_cast<float>(r);
-			foor *= dimm;
-			float foog = static_cast<float>(g);
-			foog *= dimm;
-			float foob = static_cast<float>(b);
-			foob *= dimm;
-			r = static_cast<unsigned int>(foor);
-			g = static_cast<unsigned int>(foog);
-			b = static_cast<unsigned int>(foob);
-			draw->setData(position, RGB(r, g, b));
-		}
-	}
-};
-
-void Animation::fadeMap2(datamap* draw, datamap* constant)
+// two steps vector window to fade
+void Animation::fadeMa(datamap* draw, datamap* constant)
 {
 	draw->initData("zero");
 	float value;
